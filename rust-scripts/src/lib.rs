03b8b457da80aba4f9383bf85b49edb8c4d45e28
lengyijun <sjtu5140809011@gmail.com>
2026-05-26 08:12:57 +0800

    wip

--- rust-scripts/src/lib.rs
+++ rust-scripts/src/lib.rs
@@ -6,13 +6,14 @@ use wasm_bindgen::prelude::wasm_bindgen;
 #[derive(Debug, Clone)]
 pub struct Res {
     pub encoded: String,
+    pub debruijn: String,
     pub fokker_size: usize,
     pub lc: bool,
     pub all0: bool,
     pub no_duplicate: bool,
     pub every_bvar_is_used: bool,
     pub has_beta_redex: bool,
-    pub has_eta_redex: bool,
+    pub eta_normal_form: Option<String>,
     pub two_vars_are_enough: bool,
     pub priority: String,
     pub difficulty: String,
@@ -36,15 +37,31 @@ pub fn parse_term_in_any_format(x: &str) -> Option<Res> {
     let x = x.replace("var", " ");
     let x = x.replace("lam", "λ");
 
+    let xs: Vec<_> = x.trim().chars().collect();
     let t;
 
     if let Ok(a) = parse(&x, DeBruijn) {
         t = a;
     } else if let Ok(a) = parse(&x, Classic) {
         t = a;
+    } else if let Some((a, _)) = Term::decode_fuel(&xs) {
+        t = a;
     } else {
         return None;
     }
+
+    let eta_normal_form = match t.lc() {
+        true => match t.has_eta_redex() {
+            true => {
+                let x = t.clone().eta_reduce();
+                assert!(x.lc());
+                assert_ne!(t, x);
+                Some(format!("{:?}", x))
+            }
+            false => None,
+        },
+        false => None,
+    };
 
     let priority = if t.all0() {
         "high"
@@ -64,13 +81,14 @@ pub fn parse_term_in_any_format(x: &str) -> Option<Res> {
 
     let res = Res {
         encoded: t.encode(),
+        debruijn: format!("{:?}", t),
         fokker_size: t.fokker_size(),
         lc: t.lc(),
         all0: t.all0(),
         no_duplicate: t.no_duplicate(),
         every_bvar_is_used: t.every_bvar_used(),
         has_beta_redex: t.has_beta_redex(),
-        has_eta_redex: t.has_eta_redex(),
+        eta_normal_form,
         two_vars_are_enough: t.two_vars_are_enough(),
         priority: priority.to_string(),
         difficulty: difficulty.to_string(),
